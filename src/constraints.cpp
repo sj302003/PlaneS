b0b2a16048d0d080e0644ba196c30c2b9ea43861
Siddhi Jalan <116865280+sj302003@users.noreply.github.com>
2024-07-28 11:16:34 +0530

    Update constraints.cpp

--- src/constraints.cpp
+++ src/constraints.cpp
@@ -1,43 +1,43 @@
 #include "constraints.h"
-
 #include<iostream>
 #include<vector>
 
-vector<Oceane::Nodeptr> m_nodes;
-vector<Oceane::Elementptr> m_elements;
-vector<Oceane::Nodeptr> m_boundaryElements;
-vector<vector<double>> m_loads;
-vector<vector<double>> m_fixity;
-vector<vector<double>> L;
-vector<vector<double>>  A;
-vector<vector<double>> F;
-vector<vector<double>> Breq;
-vector<vector<double>> BC;
-vector<vector<double>> S;
-vector<vector<double>> deltasupp;
-vector<vector<double>> parameters;
-vector<vector<string>> funcname;
-int nnode;
-int nmemb;
-double Fm;
-double avgforce;
-vector<int> nodenum;
-vector<int> xyz;
-int ns;
-vector<vector<double>> Br;
-vector<int> qs;
-vector<int> nodepos,reactpos;
-vector<vector<double>> f,deltacheck;
-double multiplier;
-vector<double> d,Fr;
-vector<vector<double>> d0;
-double fval;
-vector<int> sb(2);
-int sz;
-// struct ConstraintData {
-//     vector<vector<double>> BC;
-//     vector<vector<double>> F;
-// };
+ vector<Oceane::Nodeptr> m_nodes;
+ vector<Oceane::Elementptr> m_elements;
+ vector<Oceane::Nodeptr> m_boundaryElements;
+ vector<vector<double>> m_loads;
+ vector<vector<double>> m_fixity;
+ vector<vector<double>> L;
+ vector<vector<double>>  A;
+ vector<vector<double>> F;
+ vector<vector<double>> Breq;
+ vector<vector<double>> BC;
+ vector<vector<double>> S;
+ vector<vector<double>> deltasupp;
+ vector<vector<double>> parameters;
+ vector<vector<string>> funcname;
+ int nnode;
+ int nmemb;
+ double Fm;
+ double avgforce;
+ vector<int> nodenum;
+ vector<int> xyz;
+ int ns;
+ vector<vector<double>> Br;
+ vector<int> qs;
+ vector<int> nodepos,reactpos;
+ vector<vector<double>> f,deltacheck;
+ double multiplier;
+ vector<double> d,Fr;
+ vector<vector<double>> d0;
+ double fval;
+ vector<int> sb;
+ int sz;
+ vector<int> rset;
+		// struct ConstraintData {
+		// 	vector<vector<double>> BC;
+		// 	vector<vector<double>> F;
+		// };
 
 // struct ObjectiveData {
 //     vector<vector<double>> L;
@@ -47,18 +47,62 @@ int sz;
 //     int nmemb;
 // };
 
-int colm;
-int row;
-int count_dc = 0;
-int thereflg;
-double tempmatpar;
-vector<int> rest_dispdof;
-int dof, numnode=0;
-double obj_exec_time = 0;
-int order;
-double TOL = 1e-10;
-vector<double> weights;
-vector<double> values;
+/////
+ int k;
+ double *xcoord; 
+ double *ycoord; 
+ double s; 
+ double t;
+ vector<double> Nis(4);
+ vector<double> Nit(4);
+ vector<double> N(4);
+ double matX = 0;
+ double matY = 0;
+
+ vector<vector<double>> Jmat;
+ vector<double> Jmatrow(2);
+ vector<vector<double>> invJ(2, vector<double>(2, 0));
+ double detJ;
+ vector<vector<double>> Bd;
+ vector<double> Bdrow(8);
+ vector<vector<double>> Bs;
+ vector<double> Bsrow(24);
+ vector<vector<double>> Tmat;
+ vector<double> Tmatrow(24);
+ vector<vector<double>> T1mat;
+ vector<double> T1matrow(6);
+ vector<vector<double>> Dmat(3, vector<double>(3, 0));
+ vector<vector<double>> Bs1mat(3, vector<double>(24, 0));
+ vector<vector<double>> Bsmat(3, vector<double>(24, 0));
+
+ int x;
+ int y;
+
+ vector<vector<double>> A;
+ int n = A.size();
+ int m = A[0].size();
+ int rank;
+ vector<bool> row_selected(n, false);
+
+ int colm;
+ int row;
+ int count_dc = 0;
+ int thereflg;	
+ double tempmatpar;
+ vector<int> rest_dispdof;
+ int dof, numnode=0;
+ double obj_exec_time = 0;
+ int order;
+ double TOL = 1e-10;
+ vector<double> weights;
+ vector<double> values;
+ vector<BB> BB_results_mat(order);
+
+ int ele = boundry.ele;
+ double xcoord[4];
+ double ycoord[4];
+
+ BB BBresult;
 // struct BB {
 // 	double x = 0;
 // 	double y = 0;
@@ -90,9 +134,9 @@ vector<double> values;
 // 	vector<double> matpar;
 // };
 
-vector<bc> bc_arr;
-vector<node> nodes;
-vector<element> elements;
+ vector<bc> bc_arr;
+ vector<node> nodes;
+ vector<element> elements;
 
 // struct constdata {
 // 	vector<vector<double>> Amat, intmat;
@@ -108,8 +152,8 @@ vector<element> elements;
 // 	int lnpos;
 // };
 
-constdata constraint_data;
-objdata obj_data;
+ constdata constraint_data;
+ objdata obj_data;
 
 // struct Errret {
 // 	double errv;
@@ -123,11 +167,23 @@ objdata obj_data;
 // };
 
 
-double myfunc(unsigned, const double*, double*, void*);
-void myconstraint(unsigned, double*, unsigned, const double*, double*, void* );
+ double myfunc(unsigned, const double*, double*, void*);
+ void myconstraint(unsigned, double*, unsigned, const double*, double*, void* );
 // double myconstraint(unsigned, const double*, double*, void* );
+ GQVandW result;
+ GQVandW GQdata = getWeightAndValues(order);
 // struct GQVandW {
 // 	vector<double> value;
 // 	vector<double> weight;
 // };
-vector<double> knwndisp;
+
+ vector<BB> BB_results_mat;
+ vector<BB> BB_results_k2 = get_BB_results_mat(2, order, xcoord, ycoord, GQdata.value);
+ vector<BB> BB_results_k4 = get_BB_results_mat(4, order, xcoord, ycoord, GQdata.value);
+ vector<BB> BB_results_k3 = get_BB_results_mat(3, order, xcoord, ycoord, GQdata.value);
+ vector<BB> BB_results_k1 = get_BB_results_mat(1, order, xcoord, ycoord, GQdata.value);
+
+ vector<vector<double>> A; 
+
+ vector<double> knwndisp;
+#endif
