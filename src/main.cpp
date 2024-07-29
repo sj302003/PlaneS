c7d90e0209157847c81041b442c82fc11fdba1c0
Siddhi Jalan <116865280+sj302003@users.noreply.github.com>
2024-07-29 20:59:37 +0530

    main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include "read.h"
 #include "initialize.h"
 #include "solve.h"
+#include "display.h"
 #include <iostream>
+#include <vector>
 
 int main() 
 {
@@ -11,6 +13,8 @@ int main()
     init.startInitializing();
     Solve solver1;
     solver1.Solver();
+    displayResults(init.BB_results_k2, init.BB_results_k4, init.BB_results_k3, init.BB_results_k1, init.rank);
+
 
     return 0;
 }
